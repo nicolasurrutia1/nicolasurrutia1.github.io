f5a1c7a01c5de0991e99ab6ce36be4f4fada65ca
Nicolas Urrutia <nicoxo95@gmail.com>
2022-02-06 13:22:46 -0300

    rotation to photo

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,14 @@
 // document.getElementById("demo").onclick = function() {myFunction()};
 // let photoMine =document.getElementById("photoMine")
-// document.getElementById("photoMine").addEventListener("click", function(){});
-gsap.to(".photoMine", { duration: 1, rotationY: 360 ,opacity:1});    
+let photoRotation=360;
+gsap.to(".photoMine", { duration: 1, rotationY: photoRotation, opacity: 1 });
+let photoMine = document.getElementById("photoMine");
+photoMine.addEventListener("click", function (e) {
+    if(photoRotation ===360){        
+        photoRotation-=360;
+        gsap.to(".photoMine", {duration: 1, rotationY: photoRotation, opacity: 1});
+    }else{
+        photoRotation+=360;
+        gsap.to(".photoMine", {duration: 1, rotationY: photoRotation+360, opacity: 1});
+    }    
+});
